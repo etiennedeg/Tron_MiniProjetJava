4c5d3aebbd8be25b60c4175a99090374b76d2f23
Mace De Gastines Etienne <emacedegastines@asi-testing>
2016-12-02 14:11:45 +0100

    création de Thread, ajout de méthodes

--- src/tron/Partie.java
+++ src/tron/Partie.java
@@ -6,16 +6,19 @@ public class Partie {
 	public static final int TAILLE_X = 50;
 	public static final int TAILLE_Y = 20;
 	private int m_nombresDeManches;
+	private PartieThread m_partieThread;
 	private int[] m_scores;
 	private int[][] m_grille;
 	private int[] m_serpents;
+	private int m_vitesse;
 	//private Joueur[] m_joueurs;
 	private boolean m_isPartieEnCours;  //true si la partie est déjà lancée
 	//private Ecran m_ecran;
 
-	public Partie()
+	public Partie(int uneVitesse)
 	{
 		m_nombresDeManches = 0;
+		m_partieThread = new PartieThread(this);
 		for(int i = 0; i < TAILLE_X; ++i)
 		{
 			for(int j = 0; j < TAILLE_Y; ++j)
@@ -24,7 +27,46 @@ public class Partie {
 			}
 		}
 		m_isPartieEnCours = false;
+		m_vitesse = uneVitesse;
 	}
 
+	public void lancerPartie()
+	{
 
+	}
+
+	public void stopperPartie(){
+
+	}
+
+	public void stopperPartie(Joueur unJoueur){
+
+	}
+
+	public void jouerPartie()
+	{
+
+	}
+
+	private void designerGagnant(){
+
+	}
+
+	private void detruireJoueurs(){
+
+	}
+
+	private Boolean deplacerSerpent(KeyEvent e){
+
+	}
+
+	// getters et setters
+
+	public boolean isPartieEnCours(){
+		return m_isPartieEnCours;
+	}
+
+	public int getVitesse(){
+		return m_vitesse;
+	}
 }
