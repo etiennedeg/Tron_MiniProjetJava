7a3095103e671637797646ed28a6e81df12d90f5
Mace De Gastines Etienne <emacedegastines@asi-testing>
2016-12-14 19:40:38 +0100

    changement booleen retour de changerTete et isDead

--- src/tron/Serpent.java
+++ src/tron/Serpent.java
@@ -8,23 +8,23 @@ public class Serpent {
 	private int m_teteX=0;
 	private int m_score=0;
 	private int m_teteY=0;
-	private boolean m_isWin=false;
+	private boolean m_isDead=false;
 	private Joueur m_joueur;
 	private Partie m_partie;
-	
-	public void Serpent(int unNumero,String uneCouleur){
+
+	public Serpent(int unNumero,String uneCouleur){
 		m_numero=unNumero;
 		m_couleur=uneCouleur;
 	}
-	
+
 	public int getTeteX(){
 		return m_teteX;
 	}
-	
+
 	public int getTeteY(){
 		return m_teteY;
 	}
-	
+
 	public boolean changerTete(int x,int y){
 		m_teteX=x;
 		m_teteY=y;
@@ -32,53 +32,53 @@ public class Serpent {
 		if (uneGrille[x][y]==0)
 		{
 			m_partie.setGrille(x,y,m_numero);
-			return false;
-		}else{
 			return true;
+		}else{
+			return false;
 		}
 	}
-	
+
 	public boolean deplacerSerpent(){
 			if(m_orientation==1){              //gauch
 				int x=m_teteX-1;
 				int y=m_teteY;
-				m_isWin=changerTete(x,y);
+				m_isDead=changerTete(x,y);
 			}
 			else if(m_orientation==2){              //haut
 				int x=m_teteX;
 				int y=m_teteY-1;
-				m_isWin=changerTete(x,y);
+				m_isDead=changerTete(x,y);
 			}
 			else if(m_orientation==3){              //droite
 				int x=m_teteX+1;
 				int y=m_teteY;
-				m_isWin=changerTete(x,y);
+				m_isDead=changerTete(x,y);
 			}
 			else if(m_orientation==4){              //bas
 				int x=m_teteX;
 				int y=m_teteY+1;
-				m_isWin=changerTete(x,y);
+				m_isDead=!changerTete(x,y);
 		}
 		//changerScore();
-		return m_isWin;
+		return m_isDead;
 	}
-	
+
 	public void setPartie(Partie unePartie){
 		m_partie=unePartie;
 	}
-	
+
 	public int getOrientation(){
 		return m_orientation;
 	}
-	
+
 	public void changerOrientation(int uneOrientation){
 		if (java.lang.Math.abs(m_orientation-uneOrientation)!=2){
 			m_orientation=uneOrientation;
 		}
 	}
-	
+
 	public boolean getRes(){
-		return m_isWin;
+		return m_isDead;
 	}
 	public String getCouleur(){
 		return m_couleur;
