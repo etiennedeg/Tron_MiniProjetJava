6c6217375d77e3bcf63bf5d07bc1d4c5b439ca82
boblespatates <noelzablocki@yahoo.fr>
2016-12-15 15:54:43 +0100

    ajout de client

---
+++ src/tron/rmi/Client.java
@@ -0,0 +1,14 @@
+package tron.rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import tron.*;
+
+public class Client {
+	
+	
+
+}
