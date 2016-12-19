171e9b2afc87d798d8f88a359ead12e8680458fd
Mace De Gastines Etienne <emacedegastines@asi-testing>
2016-12-19 15:41:07 +0100

    création du menu pour la création d'une partie

--- src/tron/Controle.java
+++ src/tron/Controle.java
@@ -4,22 +4,62 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 public class Controle implements ActionListener {
 
 
-	public Menu m_menu;
-	public Controle (Menu menu){
-		m_menu = menu;
+
+	private Menu m_menu;
+	private Joueur m_joueur;
+	private DefaultListModel<String> m_joueursConnectes;
+	private JList<String> m_listeJoueurs;
+
+
+	public Controle (Menu unMenu, Joueur unJoueur){
+		m_menu = unMenu;
+		m_joueur = unJoueur;
+		m_joueursConnectes = new DefaultListModel<String>();
+		m_listeJoueurs = new JList<String>();
+		m_listeJoueurs.setModel(m_joueursConnectes);
 	}
+
+
 	public void actionPerformed(ActionEvent e){
-		int nombreJoueurs = Integer.parseInt(JOptionPane.showInputDialog("Combien de joueurs ? (2-4)"));
+		//demande si le joueur veut creer une partie ou en rejoindre une
+		String[] choixPartie = {"creer une nouvelle partie", "rejoindre une partie existante"};
+		int isPartieCree = JOptionPane.showOptionDialog(null, null, null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,  choixPartie, choixPartie[0]);
+		if (isPartieCree == 0){
+			creerPartie();
+		}
+		else{
+			rejoindrePartie();
+		}
+		/*int nombreJoueurs = Integer.parseInt(JOptionPane.showInputDialog("Combien de joueurs ? (2-4)"));
 		int vitesse = Integer.parseInt(JOptionPane.showInputDialog("Choisissez une vitesse svp (from 1-100)"));
 		Partie p = new Partie(nombreJoueurs,vitesse);
 		//p.lancerPartie(); le grille n'est pas bien initialise , je pense que les serpents doivent etre initialise dans la constructeur de Partie
 		this.m_menu.dispose();
 		p.m_ecran.setVisible(true);
-		p.lancerPartie();
+		p.lancerPartie();*/
 
+	}
+
+	public void creerPartie(){
+		//demande le nombre de joueurs souhaité et la vitesse du serpent
+		int nombreJoueursMax = Integer.parseInt(JOptionPane.showInputDialog("Combien de joueurs ? (2-4)"));
+		String[] choixVitesse = {"lent" , "modere", "rapide"};
+		int vitesse = JOptionPane.showOptionDialog(null, null, null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,  choixVitesse, choixVitesse[1]);
+		Partie partie = new Partie(nombreJoueursMax,vitesse*5000);
+		m_joueursConnectes.addElement(m_joueur.getNom());
+	}
+
+	public void rejoindrePartie(){
+		Partie p = new Partie(nombreJoueurs,vitesse);
+		//p.lancerPartie(); le grille n'est pas bien initialise , je pense que les serpents doivent etre initialise dans la constructeur de Partie
+		this.m_menu.dispose();
+		p.m_ecran.setVisible(true);
+		p.lancerPartie();*/
 	}
 }
