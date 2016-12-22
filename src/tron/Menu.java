e6ae6003138abf335275d0d8cc2a548985836baa
etienne <etienne.mace_de_gastines@insa-rouen.fr>
2016-12-22 12:08:03 +0100

    ajout de boutons oubliés dans le menu

--- src/tron/Menu.java
+++ src/tron/Menu.java
@@ -28,6 +28,8 @@ public class Menu extends JFrame{
 	private JButton m_buttonStart;
 	private JButton m_boutonCreerPartie;
 	private JButton m_boutonRejoindrePartie;
+	private JButton m_boutonTerminerPartie;
+	private JButton m_boutonQuitterPartie;
 
 
 
@@ -58,8 +60,10 @@ public class Menu extends JFrame{
 	}
 
 	void afficherListePartie(){
+		m_panelStart.removeAll();
+		
 		//affichage de la liste de partie
-		m_nomsPartiesCrees = new DefaultListModel<String>();
+		m_nomsPartiesCrees = new DefaultListModel<String>(); //serveur --> chercher cette liste sur le serveur
 		m_partiesCrees = new ArrayList<Partie>();
 		m_listeParties = new JList<String>();
 		m_listeParties.setModel(m_nomsPartiesCrees);
@@ -80,25 +84,20 @@ public class Menu extends JFrame{
 		m_boutonRejoindrePartie.setLocation(400,580);
 		m_panelStart.add(m_boutonRejoindrePartie);
 		m_boutonRejoindrePartie.addActionListener(m_controle);
-
-		// chercher les parties crées dans le serveur
 		
 		repaint();
 	}
 
 	void afficherListeJoueur(){
 		//affichage de la liste des joueurs
-		m_joueursConnectes = new DefaultListModel<String>();
+		m_joueursConnectes = new DefaultListModel<String>(); //serveur --> chercher cette liste sur le serveur
 		m_listeJoueurs = new JList<String>();
 		m_listeJoueurs.setModel(m_joueursConnectes);
 		m_listeJoueurs.setSize(210,210);
 		m_listeJoueurs.setLocation(30,450);
 
-		m_joueursConnectes.addElement(m_joueur.getNom()); //a la place, l'ajouter au serveur
+		m_joueursConnectes.addElement(m_joueur.getNom()); //a la place, serveur-->ajouter le joueur a la partie
 		
-		m_panelStart.remove(m_listeParties);
-		m_panelStart.remove(m_boutonCreerPartie);
-		m_panelStart.remove(m_boutonRejoindrePartie);
 		m_panelStart.add(m_listeJoueurs);
 
 		repaint();
@@ -116,26 +115,58 @@ public class Menu extends JFrame{
 		m_buttonStart.addActionListener(m_controle);
 	}
 
+	void afficherBoutonQuitterPartie(){
+		m_boutonQuitterPartie = new JButton("Quitter la partie");
+		m_boutonQuitterPartie.setSize(300,30);
+		m_boutonQuitterPartie.setLocation(400,500);
+		m_panelStart.add(m_boutonQuitterPartie);
+		m_boutonQuitterPartie.addActionListener(m_controle);
+	}
+	
+	void afficherBoutonTerminerPartie(){
+		m_boutonTerminerPartie = new JButton("Terminer la partie");
+		m_boutonTerminerPartie.setSize(300,30);
+		m_boutonTerminerPartie.setLocation(400,650);
+		m_panelStart.add(m_boutonTerminerPartie);
+		m_boutonTerminerPartie.addActionListener(m_controle);
+	}
 
 	public void creerNouvellePartie(){
+		m_panelStart.removeAll();
+		
 		int nombreJoueursMax = Integer.parseInt(JOptionPane.showInputDialog("Combien de joueurs ? (2-4)"));
 		String[] choixVitesse = {"lent" , "modere", "rapide"};
 		int vitesse = JOptionPane.showOptionDialog(null, null, null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,  choixVitesse, choixVitesse[1]);
-		m_partie = new Partie(nombreJoueursMax, vitesse*5000, m_joueur); //ajout Partie au serveur
+		m_partie = new Partie(nombreJoueursMax, (4-vitesse)*15, m_joueur); 
+		// serveur --> ajouter la Partie et mettre le createur dans la partie
 		
 		afficherListeJoueur();
+		afficherBoutonTerminerPartie();
 		afficherBoutonStart();
 	}
 
 	public void rejoindrePartie(){
 		m_partie = m_partiesCrees.get( m_listeParties.getAnchorSelectionIndex() );
 		afficherListeJoueur();
+		afficherBoutonQuitterPartie();
+	}
+	
+	public void quitterPartie(){
+		m_panelStart.removeAll();
+		
+		// serveur --> enlever le joueur de la partie
+		afficherListePartie();
+	}
+
+	public void terminerPartie(){
+		// serveur --> faire appel à tous les joueurs de la partie quitterPartie() puis supprimer la partie
 	}
 	
 	public void lancerPartie(){
-		
+		setVisible(false);
+		m_partie.lancerPartie();
 	}
-
+	
 	public static void main (String[] args){
 		Menu menu = new Menu();
 		menu.setVisible(true);
@@ -160,7 +191,15 @@ public class Menu extends JFrame{
 			}
 
 			else if (e.getActionCommand() == "Rejoindre une partie"){
-				rejoindrePartie(); // gerer exception si aucune partie selectionÃ©e ou si partie remplie
+				rejoindrePartie(); // gerer exception si aucune partie selectionee ou si partie remplie
+			}
+			
+			else if (e.getActionCommand() == "Quitter la partie"){
+				quitterPartie();
+			}
+			
+			else if (e.getActionCommand() == "Terminer la partie"){
+				terminerPartie();
 			}
 			
 			else {
